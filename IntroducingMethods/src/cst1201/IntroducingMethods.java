ad3c7fc031c741781d153a37bc7e0330da60ab05
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-04-20 17:21:50 -0400

    Updated some documentation.

--- IntroducingMethods/src/cst1201/IntroducingMethods.java
+++ IntroducingMethods/src/cst1201/IntroducingMethods.java
@@ -14,21 +14,30 @@ public class IntroducingMethods {
         // TODO: Fill in the body with your code
     }
 
-    // Given a Scanner, prompt the user for a String.  If the user enters an empty
-    // String, report an error message and ask for a non-empty String.  Return the
-    // String to the calling program.
+    /**
+     * Given a Scanner, prompts the user for a String.  If the user enters an empty
+     * String, reports an error message and asks for a non-empty String.  Returns the
+     * String to the calling program.
+     * @param inScanner The Scanner to use to prompt the user.
+     * @return The resulting String from the user.
+     */
     private static String getInputString(Scanner inScanner) {
         // TODO: Fill in the body
 
         // NOTE: Do not declare a Scanner in the body of this method.
     }
 
-    // Given a String return the number of words in the String.  A word is a sequence of 
-    // characters with no spaces.  Write this method so that the function call:
-    // 		int count = getWordCount("The quick brown fox jumped");
-    // results in count having a value of 5.  You will call this method from the main method.
-    // For this assignment you may assume that
-    // words will be separated by exactly one space.
+    /**
+     * Given a String, returns the number of words in the String.  A word is a sequence of 
+     * characters with no spaces.  For example, the method call:
+     * <pre>
+     *      int count = getWordCount("The quick brown fox jumped");
+     * </pre>
+     * results in count having a value of 5. This method should be called from the main method.
+     * For this assignment, you may assume that words will be separated by exactly one space.
+     * @param input The String in question.
+     * @return The number of words in the String. Each word is separated by a single space.
+     */
     private static int getWordCount(String input) {
         // TODO: Fill in the body
     }
