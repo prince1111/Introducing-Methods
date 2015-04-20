b21ad7415bfcfc6287b4cf5c135945d4f040b084
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-04-20 15:58:43 -0400

    Initial code.

---
+++ IntroducingMethods/src/cst1201/IntroducingMethods.java
@@ -0,0 +1,35 @@
+package cst1201;
+
+import java.util.Scanner;
+
+/**
+ * TODO: Description of your class.
+ *
+ * @author TODO: Your name here.
+ */
+public class IntroducingMethods {
+
+    public static void main(String[] args) {
+
+        // TODO: Fill in the body with your code
+    }
+
+    // Given a Scanner, prompt the user for a String.  If the user enters an empty
+    // String, report an error message and ask for a non-empty String.  Return the
+    // String to the calling program.
+    private static String getInputString(Scanner inScanner) {
+        // TODO: Fill in the body
+
+        // NOTE: Do not declare a Scanner in the body of this method.
+    }
+
+    // Given a String return the number of words in the String.  A word is a sequence of 
+    // characters with no spaces.  Write this method so that the function call:
+    // 		int count = getWordCount("The quick brown fox jumped");
+    // results in count having a value of 5.  You will call this method from the main method.
+    // For this assignment you may assume that
+    // words will be separated by exactly one space.
+    private static int getWordCount(String input) {
+        // TODO: Fill in the body
+    }
+}
